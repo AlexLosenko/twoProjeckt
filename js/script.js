5f5df1bf45b1d869404bcd49fa80bd420474404e
AlexLosenko <loal2412@gmail.com>
2020-10-18 16:30:55 +0300

    18.10.2020

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 console.log('hi!');
+let a;
+a = 12;
+a++;
+a *= 5;
+let b = 56;
+a = b - a;
+console.log(a);
+
+let name = '';
+let lastName = 'Losenko';
+console.log(name || lastName);
+let metaD = lastName + ' ' + a;
+console.log(metaD);
+a *= 10;
+c = metaD + ': ' + a;
+console.log(c);
+
+
 // ['HI', 'Alex', '!'].forEach(alert);
